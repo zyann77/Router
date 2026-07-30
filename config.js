900b296ae9b7b44516482d6137a4aaf677356a34
zyann77 <rnbnetworksubang@gmail.com>
2026-07-30 11:33:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Ganti dengan URL dan Anon Key dari Project Supabase Anda
-const SUPABASE_URL = 'https://nrltmsyuunchfxxgwxgy.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_-FMcGyE40plsRofjFUeFzw_XfTNF9o-';
+const SUPABASE_URL = 'https://vujqghlabvxqcysezrwd.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_ysCPBYisxbUuE-uG1y734w_uUWjkBHP';
